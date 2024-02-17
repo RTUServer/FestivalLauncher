d275bdf68e615fd12d4e93067c4700450c107d71
IPECTER <ipectert@gmail.com>
2024-02-18 01:42:48 +0900

    0.0.2

--- app/assets/js/scripts/settings.js
+++ app/assets/js/scripts/settings.js
@@ -1436,7 +1436,7 @@ function populateAboutVersionInformation(){
  */
 function populateReleaseNotes(){
     $.ajax({
-        url: 'https://github.com/dscalzi/HeliosLauncher/releases.atom',
+        url: 'https://github.com/RTUServer/FestivalLauncher/releases.atom',
         success: (data) => {
             const version = 'v' + remote.app.getVersion()
             const entries = $(data).find('entry')
